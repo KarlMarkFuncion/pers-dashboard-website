03a9fb94f82ace18ef92c3d3baa3b587b8062631
Karl Mark Funcion <77195125+KarlMarkFuncion@users.noreply.github.com>
2024-01-04 18:44:31 +0800

    first_commit

---
+++ frontend/src/components/App.js
@@ -0,0 +1,18 @@
+import Nav from "./reused_elements/Nav";
+
+ 
+function App() {
+  return (
+    <div className="App">
+      <div className="lg">
+        <Nav />
+        <div className="dark-bg">
+          Hello
+        </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
