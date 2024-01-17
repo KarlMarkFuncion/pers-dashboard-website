46ed9cf86c809ead7b43a3165f0e68601aa56bdf
km_func <karlmark.funcion@gmail.com>
2024-01-17 09:37:12 +0800

    sidebar and nav bar addition

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,12 +1,14 @@
 import Nav from "./reused_elements/Nav";
+import SideBar from "./reused_elements/Sidebar";
 
  
 function App() {
   return (
     <div className="App">
-      <div className="lg">
+      <div className="lg  h-full">
         <Nav />
-        <div className="dark-bg">
+        <div className="dark-bg lg flex gap-5 h-100">
+          <SideBar />
           Hello
         </div>
       </div>
---
+++ frontend/src/components/reused_elements/Sidebar.js
@@ -0,0 +1,49 @@
+
+'use client';
+
+import { Sidebar } from 'flowbite-react'; 
+
+const SideBar = () => {
+  return (
+    <Sidebar aria-label="Sidebar with content separator example" className='border-r-2 h-100'>
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Sidebar.Item href="#">
+            Dashboard
+          </Sidebar.Item>
+          <Sidebar.Item href="#">
+            Kanban
+          </Sidebar.Item>
+          <Sidebar.Item href="#">
+            Inbox
+          </Sidebar.Item>
+          <Sidebar.Item href="#">
+            Users
+          </Sidebar.Item>
+          <Sidebar.Item href="#">
+            Products
+          </Sidebar.Item>
+          <Sidebar.Item href="#">
+            Sign In
+          </Sidebar.Item>
+          <Sidebar.Item href="#">
+            Sign Up
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+        <Sidebar.ItemGroup>
+          <Sidebar.Item href="#">
+            Upgrade to Pro
+          </Sidebar.Item>
+          <Sidebar.Item href="#">
+            Documentation
+          </Sidebar.Item>
+          <Sidebar.Item href="#">
+            Help
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+}
+
+export default SideBar
